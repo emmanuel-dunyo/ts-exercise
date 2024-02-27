aef0d85e5405f9266547e9115c19d887556da516
Emmanuel Dunyo <emmanuel.dunyo@whitelabel-loyalty.com>
2024-02-27 15:33:04 +0000

    complicated getDescendants

--- src/Node.test.ts
+++ src/Node.test.ts
@@ -1,6 +1,6 @@
 import { Node } from "./Node";
 
-describe('Item class', () => {
+describe('Node class', () => {
 
   it('test addChild', () => {
     const sikhism = new Node();
@@ -10,7 +10,7 @@ describe('Item class', () => {
     expect(sikhism.hasChild(india)).toBeTruthy();
     expect(india.hasChild(sikhism)).toBeFalsy();
   });
-  
+
   it('test getChildren', () => {
     const budhism = new Node();
     const india = new Node();
@@ -21,21 +21,17 @@ describe('Item class', () => {
     budhism.addChild(england);
     budhism.addChild(japan);
     expect(budhism.getChildren().size).toBe(3);
-
   });
 
-  it('test getChildren adding the same child twice', () => {
-    const budhism = new Node();
-    const india = new Node();
+  //!
+  // it('test getChildren adding the same child twice', () => {
+  //   const budhism = new Node();
+  //   const india = new Node();
 
-    budhism.addChild(india);
-    budhism.addChild(india);
-    expect(budhism.getChildren().size).toBe(1);
-
-  });
-
-
-
+  //   budhism.addChild(india);
+  //   budhism.addChild(india);
+  //   expect(budhism.getChildren().size).toBe(1);
+  // });
   
   it('test returned collection is immutable', () => {
     const budhism = new Node();
@@ -47,9 +43,75 @@ describe('Item class', () => {
 
     children.add(new Node());
     expect(children.size).toBe(2);
-
     expect(budhism.getChildren().size).toBe(1)
   });
 
+  it('test getDescendants()', () => {
+    const parent = new Node();
+    const child = new Node();
+    const grandchild = new Node();
 
+    parent.addChild(child);
+    child.addChild(grandchild);
+
+    expect(parent.getDescendants().size).toBe(2);
+    expect(parent.getDescendants()).toContain(child);
+    expect(parent.getDescendants()).toContain(grandchild);
+  });
+  
+  it('test getDescendants() a bit more complicated', () => {
+    const parent = new Node();
+    const child1 = new Node();
+    const child2 = new Node()
+    const child3 = new Node()
+    const grandchild = new Node();
+    const greatgrandchild = new Node();
+
+    parent.addChild(child1);
+    parent.addChild(child2);
+    parent.addChild(child3);
+    child2.addChild(grandchild);
+    grandchild.addChild(greatgrandchild)
+
+    expect(parent.getDescendants().size).toBe(5);
+    expect(parent.getDescendants()).toContain(child1);
+    expect(parent.getDescendants()).toContain(child2);
+    expect(parent.getDescendants()).toContain(child3);
+    expect(parent.getDescendants()).toContain(grandchild);
+    expect(parent.getDescendants()).toContain(greatgrandchild);
+  });
+
+  it('jobber', () => {
+    const parent = new Node();
+    const child = new Node();
+    
+    parent.addChild(child)
+    // child.addChild(parent)
+    // expect( function() {child.addChild(parent);}).toThrow(new Error("Adding this child would create a cyclic link."))
+
+    // console.log(add)
+
+    
+    // detect cyclic DAG loop expection
+    // try catch throw new error to prevent it from creating a cyclic loop  
+    
+    // try {
+      //   child.addChild(parent)
+      // } catch {
+        //   throw new Error("Don't do it.")
+        // }
+        
+      // if (child.getDescendants().has(parent)) {
+      //     throw new Error("Don't do it.")
+      //     null
+      // } else {
+      //   child.addChild(parent)
+      // }
+
+      // if (parent.getDescendants().has(child)) {
+      //   throw new Error('NOPE');
+      // }
+
+    // expect(parent.getDescendants().size).toBe(1);
+  });
 });
--- src/Node.ts
+++ src/Node.ts
@@ -1,15 +1,34 @@
+
+// DAG - Directed Acyclic Graph
 export class Node {
     private children = new Set<Node>()
-    
+
     hasChild(item: Node): boolean {
         return this.children.has(item);
     }
-    
+
     addChild(item: Node) {
-            this.children.add(item)
+        // if (this.getDescendants().has(item)) {
+        //     throw new Error("Adding this child would create a cyclic link.");
+        // }
+        this.children.add(item);
+        const jobber = this.getDescendants()
+        console.log('getDesc', this.getDescendants())
     }
-    
+
     getChildren(): Set<Node> {
         return new Set<Node>(this.children)
     }
+
+    getDescendants(): Set<Node> {
+        const result: Set<Node> = new Set();
+
+        this.children.forEach((child) => {
+            result.add(child);
+            child.getDescendants().forEach(descendant => result.add(descendant));
+        });
+
+        return result;
+    }
+    // this returns children as well as daeacendants - add one child to one parent and getDescendants will be true for parent to contain child
 }
