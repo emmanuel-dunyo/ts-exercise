9b6faf50689354968940fb598148daff45fd4ff7
Emmanuel Dunyo <emmanuel.dunyo@whitelabel-loyalty.com>
2024-11-06 15:57:56 +0000

    added check for duplicate children

--- src/DAGs/NodeMap/NodeMap.test.ts
+++ src/DAGs/NodeMap/NodeMap.test.ts
@@ -1,18 +1,29 @@
 import { NodeMap } from './NodeMap';
 
+let dag: NodeMap<String>;
+
+beforeEach(() => {
+  dag = new NodeMap<String>();
+});
+
 describe('Node Map class', () => {
   it('Create a simple DAG of strings "Parent" -> "Child"', () => {
-    const dag = new NodeMap<String>();
     dag.addChildToParent('Parent', 'Child');
     const children = dag.getChildrenOfNode('Parent');
     expect(children).toContain('Child');
   });
 
   it('Create a simple DAG of strings "Parent" -> "Child1" and "Child2', () => {
-    const dag = new NodeMap<String>();
     dag.addChildToParent('Parent', 'Child');
     dag.addChildToParent('Parent', 'Child2');
     const children = dag.getChildrenOfNode('Parent');
     expect(children.length).toBe(2);
+  });
+
+  it('Add the same child twice..', () => {
+    dag.addChildToParent('Parent', 'Child');
+    dag.addChildToParent('Parent', 'Child');
+    const children = dag.getChildrenOfNode('Parent');
+    expect(children.length).toBe(1);
   });
 });
--- src/DAGs/NodeMap/NodeMap.ts
+++ src/DAGs/NodeMap/NodeMap.ts
@@ -2,9 +2,17 @@ export class NodeMap<T> {
   private map: Map<T, Array<T>> = new Map();
 
   addChildToParent(parent: T, child: T) {
-    const childrenArray = this.map.get(parent) || new Array<T>();
+    const childrenArray = this.map.get(parent) ?? new Array<T>();
+
+    if (!this.map.has(parent)) {
+      this.map.set(parent, childrenArray);
+    }
+
+    if (this.map.get(parent)?.includes(child)) {
+      return;
+    }
+
     childrenArray.push(child);
-    this.map.set(parent, childrenArray);
   }
 
   getChildrenOfNode(parent: T): Array<T> {
